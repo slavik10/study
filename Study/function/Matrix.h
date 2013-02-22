18d939b8d02a8e7eeaa693f485468873b6d8baed
slavik10 <dmitrsl10@gmail.com>
2013-02-22 15:03:01 +0400

    upd

--- Study/function/Matrix.h
+++ Study/function/Matrix.h
@@ -4,15 +4,15 @@ void ChangeVariables(int* a, int* b);
 void ChangeArrays(int* arr, int* arr2, int size);
 void ChangeArrays(int* arr, int* arr2, int size, int step);
 
-void WriteArray(int* arr, int size);
-void WriteArray(int* arr, int size, int step);
+void PrintArray(int* arr, int size);
+void PrintArray(int* arr, int size, int step);
 
 bool ArrayIsAsc(int* arr, int size);
 bool ArrayIsAsc(int* arr, int size, int step);
 bool ArrayIsDesc(int* arr, int size);
 bool ArrayIsDesc(int* arr, int size, int step);
 
-void WriteMatrix(int* arr, int row, int col);
+void PrintMatrix(int* arr, int row, int col);
 
 int FindMinInMatrix(int* arr, int row, int col);
 int FindMaxInMatrix(int* arr, int row, int col);
